e27020ba6a84e1b6227525ea76af7f8c06aa7eaf
Nick Sagona <nick@nolainteractive.com>
2016-10-04 11:01:51 -0500

    Update docblocks

--- src/Csv.php
+++ src/Csv.php
@@ -17,7 +17,7 @@ namespace Pop\Csv;
  * CSV class
  *
  * @category   Pop
- * @package    Pop_Data
+ * @package    Pop\Csv
  * @author     Nick Sagona, III <dev@nolainteractive.com>
  * @copyright  Copyright (c) 2009-2016 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
